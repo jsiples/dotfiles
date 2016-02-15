de08563c5f82ead19ec96d54bfdf7bc62d338fbb
Justin Siples <jsiples@gmail.com>
2016-02-15 15:15:49 -0500

    Updates to muttrc, cmus, awesome

---
+++ .cmus/playlist.pl
@@ -0,0 +1,20 @@
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/01 - Evolution Continues.mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/02 - Imaginary.mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/03 - Miles (End Credit Song).mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/04 - Never Break Me (Toneshifterz Remix).mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/05 - We Come & We Go (We R Eos Mix).mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/06 - Tonight Will Never Die (Edit).mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/07 - Privilege 2 Born Again.mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/08 - Scrap the System (Defqon.1 Australia Anthem'13).mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/09 - Wide Awake (Eos 2013 Mix).mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/10 - F.I.F.O..mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/11 - Life that We Dream of (City 2 City).mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/12 - We Can Escape (Intents Anthem 2012).mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/13 - Fight the Resistance (Edit).mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/14 - Lose My Mind.mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/15 - Hardbass Junkie.mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/16 - Freqshow Anthem (Radio Edit).mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/17 - Running Late (Brennan Heart & Code Black Mf Earthquake Rawmix).mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/18 - F.I.F.O. (Outbreak Mix).mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/19 - Evolution of Style (Eos Intro).mp3
+/home/jsiples/Music/Brennan Heart - Evolution of Style (2014)/20 - A Scattered Soul.mp3
